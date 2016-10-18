28512d6faad7a3d3b09502539dce0b9c3bb63884
Renee Chu <renee.achu@gmail.com>
2016-10-18 12:39:00 -0700

    break out JS into seperate files, add event listeners and reload() function def

---
+++ javascript/javascript.js
@@ -0,0 +1,44 @@
+function authenticated() {
+	return Boolean(getCredentials());
+}
+
+function getCredentials() {
+	if (!sessionStorage.getItem('userId')) {
+		return null;
+	}
+	return {
+		userId: sessionStorage.getItem('userId'),
+		password: sessionStorage.getItem('password'),
+		username: sessionStorage.getItem('username')
+	};
+}
+
+function makeRequest(method, url, credentials, body) {
+	const headers = new Headers();
+
+	if (method === 'POST') {
+		headers.append('Content-Type', 'application/json');
+	}
+	if (credentials) {
+		const {userId, password} = credentials;
+		headers.append('Authorization', 'Basic ' + btoa(`${userId}:${password}`));	
+	}
+	
+	return fetch(url, {
+		method: method,
+		headers: headers,
+		body: JSON.stringify(body)
+	}).then((response) => {
+		if (response.status == 401) {
+			const message = 'Please log in with valid credentials';
+			alert(message);
+			throw new Error(message);
+		}
+		return response.json();
+	});	
+}
+
+function reload() {
+	var event = new CustomEvent('reload', {bubbles: true});
+	window.dispatchEvent(event);
+}
---
+++ javascript/loadTweets.js
@@ -0,0 +1,29 @@
+window.addEventListener('reload', function (e) { 
+	if (authenticated()) {
+		loadTweets();
+	} else {
+		removeTweets();
+	}
+});
+
+function removeTweets() {
+	window.tweets.innerHTML = '';
+}
+
+function loadTweets() {
+	var credentials = getCredentials()
+	return makeRequest('GET', 'http://localhost:5000/tweets', credentials)
+		.then((jsonResponse) => {
+			window.tweets.innerHTML = '';
+			for (const tweet of jsonResponse.tweets) {
+				window.tweets.innerHTML += 
+				`
+				<li>
+					<p>${tweet.date_created}</p>
+					<p>${tweet.body}</p>
+					<p>${tweet.username}</p>					
+				</li>
+				`
+			}
+		});
+};
