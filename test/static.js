526c219e81edc4b64edb0c26c50bb3f0712d3b97
Renee Chu <renee.achu@gmail.com>
2016-10-18 23:00:27 -0700

    visit browser in tests

--- test/static.js
+++ test/static.js
@@ -1,5 +1,16 @@
 import test from 'ava';
+import PhantomJS from 'phantomjs-adapter';
 
-test((t) => {
-	t.pass();
+const browser = new PhantomJS();
+
+test.before(async () => {
+	// python -m SimpleHTTPServer 8080 
+	await browser.open('http://127.0.0.1:8080')
+});
+
+test(async (t) => {
+	const usernameField = browser.find('input[name="userId"]');
+	const passwordField = browser.find('input[name="password"]');
+
+	
 });
