2eab8080aa7414ef8b8bdd1b9090c3fe6f38b314
Renee Chu <renee.achu@gmail.com>
2016-10-30 21:41:52 -0700

    replace phantomjs with selenium-webdriver in test

--- test/static.js
+++ test/static.js
@@ -1,71 +1,84 @@
 import test from 'ava';
-import PhantomJS from 'phantomjs-adapter';
+import webdriver, {By, until} from 'selenium-webdriver';
 
-const browser = new PhantomJS();
+const browser = new webdriver.Builder()
+							.forBrowser('chrome')
+							.build();
+	
 
-browser.logs.forEach((log) => {
-  console.log(log);
+test.before(async (t) => {
+	await browser.get('http://127.0.0.1:8080');
 });
 
-test.before(async () => {
-	await browser.open('http://127.0.0.1:8080');
-});
-
-test.after.always(async() => {
-	await browser.exit();
+test.after.always(async(t) => {
+	await browser.quit();
 })
 
-// test.only('submitting bad credentials does not log in and clears sessionStorage', async (t) => {
-// 	const postTweetField = await browser.find('textarea[name="body"]');
-// 	t.is(postTweetField, null);
-// 	const tweetsListTable = await browser.find('li[class="tweet"]');
-// 	t.is(tweetsListTable, null);
+test.only('submitting bad credentials does not log in and clears sessionStorage', async (t) => {
+	const postTweetField = await browser.findElements(By.css('textarea[name="body"]'));
+	const tweetsListTable = await browser.findElements(By.css('li[class="tweet"]'));
+	t.is(postTweetField.length, 0);
+	t.is(tweetsListTable.length, 0);
 
-// 	const usernameField = await browser.find('input[name="userId"]');	
-// 	const passwordField = await browser.find('input[name="password"]');
+	const sessionStorageUserId = await browser.executeScript(() => window.sessionStorage.userId);
+	const sessionStorageUserPassword = await browser.executeScript(() => window.sessionStorage.password);
+	t.is(sessionStorageUserId, null)
+	t.is(sessionStorageUserPassword, null)
 
-// 	console.log('Session Storage Before:');	
-// 	await browser.evaluate("console.log('Hello World')");
-// 	// await browser.evaluate('console.log(Object.values(window.sessionStorage))');
+	const usernameField = await browser.findElement(By.css('input[name="userId"]'));	
+	const passwordField = await browser.findElement(By.css('input[name="password"]'));
+	
+	await usernameField.sendKeys('BadUsername');
+	await passwordField.sendKeys('BadPassword');
 
-// 	await usernameField.fillIn('BadUsername');
-// 	await passwordField.fillIn('BadPassword');
-
-// 	console.log('Session Storage After:');	
-// 	await browser.evaluate(`\
-// 	var sessionStorage = window.sessionStorage;
-//  	var keys = $.map(sessionStorage, function(v, i) {
-// 		return i;
-// 	});
-
-// 	console.log(keys);
-
-// 	`);
-// 	// await browser.evaluate('console.log(Object.values(window.sessionStorage))');
-
-// 	const submitButton = await browser.find('button[type="submit"]')
-// 	await submitButton.click({wait: 2000});
-
-// });
-
-test('logging in loads post-tweet and load-tweet fields', async (t) => {
-	const postTweetField = await browser.find('textarea[name="body"]');
-	t.is(postTweetField, null);
-	const tweetsListTable = await browser.find('li[class="tweet"]');
-	t.is(tweetsListTable, null);
-
-	const usernameField = await browser.find('input[name="userId"]');	
-	const passwordField = await browser.find('input[name="password"]');
-
-	await usernameField.fillIn('US508f050371364c');
-	await passwordField.fillIn('HDY2JT5IJRIQPYIP');
-
-	const submitButton = await browser.find('button[type="submit"]')
+	const submitButton = await browser.findElement(By.css('button[type="submit"]'));
+	// stub out alert button
+	await browser.executeScript("window.alert = function(msg){return true;};");
 	await submitButton.click();
 
-	const postTweetFieldLoaded = await browser.find('textarea[name="body"]', {wait: 2000});
-	console.log((await browser.find('body')).innerHTML);
-	t.not(postTweetFieldLoaded, null);
-	const tweetsListTableLoaded = await browser.find('li[class="tweet"]');
-	t.not(tweetsListTableLoaded, null);
+	await browser.executeScript(() => console.log('Wasting time'));
+
+	const sessionStorageUserIdAfter = await browser.executeScript(() => window.sessionStorage.userId);
+	const sessionStorageUserPasswordAfter = await browser.executeScript(() => window.sessionStorage.password);
+	t.is(sessionStorageUserIdAfter, null)
+	t.is(sessionStorageUserPasswordAfter, null)
+
+	const postTweetFieldLoaded = await browser.findElements(By.css('textarea[name="body"]'));
+	const tweetsListTableLoaded = await browser.findElements(By.css('li[class="tweet"]'));
+	t.is(postTweetField.length, 0);
+	t.is(tweetsListTable.length, 0);
+});
+
+test('logging in loads post-tweet and load-tweet fields and saves login info to sessionStorage', async (t) => {
+	const postTweetField = await browser.findElements(By.css('textarea[name="body"]'));
+	const tweetsListTable = await browser.findElements(By.css('li[class="tweet"]'));
+	t.is(postTweetField.length, 0);
+	t.is(tweetsListTable.length, 0);
+
+	let sessionStorageUserId = await browser.executeScript(() => window.sessionStorage.userId);
+	let sessionStorageUserPassword = await browser.executeScript(() => window.sessionStorage.password);
+	t.is(sessionStorageUserId, null)
+	t.is(sessionStorageUserPassword, null)
+
+	const usernameField = await browser.findElement(By.css('input[name="userId"]'));	
+	const passwordField = await browser.findElement(By.css('input[name="password"]'));
+	
+	await usernameField.sendKeys('US508f050371364c');
+	await passwordField.sendKeys('HDY2JT5IJRIQPYIP');
+
+	const submitButton = await browser.findElement(By.css('button[type="submit"]'));
+	await submitButton.click();
+
+	await browser.wait(until.elementLocated(By.css('textarea[name="body"]')));
+	await browser.wait(until.elementLocated(By.css('li[class="tweet"]')));
+
+	const postTweetFieldLoaded = await browser.findElements(By.css('textarea[name="body"]'));
+	const tweetsListTableLoaded = await browser.findElements(By.css('li[class="tweet"]'));
+	t.is(postTweetFieldLoaded.length, 1);
+	t.true(tweetsListTableLoaded.length > 1);
+
+	sessionStorageUserId = await browser.executeScript(() => window.sessionStorage.userId);
+	sessionStorageUserPassword = await browser.executeScript(() => window.sessionStorage.password);
+	t.is(sessionStorageUserId, 'US508f050371364c')
+	t.is(sessionStorageUserPassword, 'HDY2JT5IJRIQPYIP')
 });
